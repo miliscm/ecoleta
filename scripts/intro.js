9398451ebfdd0a24f55196722b8fd6df6585ce56
Milena Mocci <miliscm@gmail.com>
2020-06-04 15:09:56 -0300

    primeiro projeto

---
+++ scripts/intro.js
@@ -0,0 +1,75 @@
+// comments
+// document.write("hello")
+//variáreis, tipos de dados
+// let myvar = "he"
+// const myconst = "he"
+// document.write(myconst + myvar)
+
+//string
+// const s1 ="isso é string"
+// const s2 = 'isso é string sim'
+// const s3 = `isso é string também`
+
+// document.write("isso é string")
+
+//number
+// const n2 = "12"
+// const n1 = 1
+
+// document.write( n1 + n2)
+
+//boolean true ou false
+// const bTrue = true
+
+// const bFalse = false
+
+// document.write(bTrue)
+
+//objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura:"1.60cm",
+//   idade: 37,
+//   solteiro: true,
+//   correr(){
+//    document.write("executar")
+//     // return "run forest"
+//   }
+// }
+// // document.write(pessoa.correr())
+// pessoa.correr()
+
+//array ou vetores
+// // [.....] coleção de alguma coisa
+
+// const colecaodeBolinhas = ["blue", "green", 1, {name:"milena"}]
+
+// document.write(colecaodeBolinhas[3].name)
+
+
+//Função
+// registrar função
+// function sayMyname(name){
+//   document.write(name)
+// }
+// // executa a função
+// sayMyname("Joao")
+// sayMyname("Milena")
+// sayMyname("André")
+
+
+// condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal >= 7){
+//   document.write("Aprovado")
+// }else{
+//   document.write("reprovado")
+// }
+
+
+//repetições
+
+// for(i =0; i<=10 ; i++){
+// document.write(`<p> ${i} </p>`)
+// }
